8547e1fb3d95fe1df36c6f8d9e1bf3456b882ce9
Maria Sorokina <ruafelianna@ya.ru>
2025-04-04 15:37:25 +0300

    Simplify and unify Port tests even more

--- tests/Services/test_port.py
+++ tests/Services/test_port.py
@@ -9,6 +9,14 @@ from typing import Literal
 from SilvaViridis.Python.DockerComposeBuilder.Common import AppProtocol, Configuration
 from SilvaViridis.Python.DockerComposeBuilder.Services import Port, TPort, TPortRange
 
+from ..fixtures import (
+    check_create_full,
+    check_repr_full,
+    create_obj_from_dict,
+)
+
+LABELS = ["container_port", "host_port", "host_ip", "protocol", "app_protocol", "name", "mode", "force_long_syntax"]
+
 type TCpt = TPort | TPortRange
 type THpt = TPort | TPortRange | None
 type THip = IPv4Address | IPv6Address | None
@@ -16,71 +24,14 @@ type TPrt = Literal["tcp", "udp"] | None
 type TNme = str | None
 type TApr = AppProtocol | None
 type TMde = Literal["host", "ingress"] | None
-TAll = tuple[TCpt, THpt, THip, TPrt, TNme, TApr, TMde, bool]
+TAll = tuple[TCpt, THpt, THip, TPrt, TApr, TNme, TMde, bool]
 
-container_ports = [80, (8000, 8010), (8090, 8010), (12000, 13000)]
+def create(args : TAll) -> Port:
+    return create_obj_from_dict(Port, LABELS, *args)
 
-host_ports = [8080, (9000, 9010), (9090, 9010), (14000, 15000)]
+def valid(port : TAll) -> bool:
+    container_port, host_port, _, _, _, _, _, _ = port
 
-host_ips = [IPv4Address("192.168.0.1"), IPv6Address("::1"), None]
-
-protocols = ["tcp", None]
-
-names = ["mead", None]
-
-app_protocols = [AppProtocol.http, None]
-
-modes = ["host", None]
-
-force_long_syntax_options = [False]
-
-prod_all = list(product(container_ports, host_ports, host_ips, protocols, names, app_protocols, modes, force_long_syntax_options))
-
-cp0 = container_ports[0]
-cp1 : TPortRange = container_ports[1] # type: ignore
-hp0 = host_ports[0]
-hp1 : TPortRange = host_ports[1] # type: ignore
-hi0 = host_ips[0]
-hi1 = host_ips[1]
-pr0 = protocols[0]
-nm0 = names[0]
-ap0 : AppProtocol = app_protocols[0] # type: ignore
-md0 = modes[0]
-
-full_ports = [
-    ((cp0, None, None, None, None, None, None, False), f"{cp0}"),
-    ((cp1, None, None, None, None, None, None, False), f"{cp1[0]}-{cp1[1]}"),
-    ((cp0, hp0, None, None, None, None, None, False), f"{hp0}:{cp0}"),
-    ((cp1, hp1, None, None, None, None, None, False), f"{hp1[0]}-{hp1[1]}:{cp1[0]}-{cp1[1]}"),
-    ((cp0, hp1, None, None, None, None, None, False), f"{hp1[0]}-{hp1[1]}:{cp0}"),
-    ((cp1, None, hi0, None, None, None, None, False), f"{hi0}:{cp1[0]}-{cp1[1]}"),
-    ((cp1, hp1, hi0, None, None, None, None, False), f"{hi0}:{hp1[0]}-{hp1[1]}:{cp1[0]}-{cp1[1]}"),
-    ((cp1, None, hi1, None, None, None, None, False), f"{hi1}:{cp1[0]}-{cp1[1]}"),
-    ((cp1, None, None, pr0, None, None, None, False), f"{cp1[0]}-{cp1[1]}/{pr0}"),
-    ((cp0, hp0, hi0, pr0, nm0, ap0, md0, False), {
-        "name": nm0,
-        "target": f"{cp0}",
-        "host_ip": f"{hi0}",
-        "published": f"{hp0}",
-        "protocol": pr0,
-        "app_protocol": ap0.name,
-        "mode": md0,
-    }),
-]
-
-def create(container_port : TCpt, host_port : THpt, host_ip : THip, protocol : TPrt, name : TNme, app_protocol : TApr, mode : TMde, force_long_syntax : bool):
-    return Port(
-        container_port = container_port,
-        host_port = host_port,
-        host_ip = host_ip,
-        protocol = protocol,
-        name = name,
-        app_protocol = app_protocol,
-        mode = mode,
-        force_long_syntax = force_long_syntax,
-    )
-
-def valid(container_port : TCpt, host_port : THpt, host_ip : THip, protocol : TPrt, name : TNme, app_protocol : TApr, mode : TMde, force_long_syntax : bool):
     if (
         isinstance(container_port, tuple)
         and container_port[0] >= container_port[1]
@@ -111,39 +62,88 @@ def valid(container_port : TCpt, host_port : THpt, host_ip : THip, protocol : TP
 
     return True
 
-valid_ports = [t for t in prod_all if valid(*t)] # type: ignore
+container_port_values = [80, (8000, 8010), (8090, 8010), (12000, 13000)]
 
-invalid_ports = [t for t in prod_all if not valid(*t)] # type: ignore
+host_port_values = [8080, (9000, 9010), (9090, 9010), (14000, 15000)]
+
+host_ip_values = [IPv4Address("192.168.0.1"), IPv6Address("::1"), None]
+
+protocol_values = ["tcp", None]
+
+name_values = ["mead", None]
+
+app_protocol_values = [AppProtocol.http, None]
+
+mode_values = ["host", None]
+
+force_long_syntax_values = [False]
+
+prod_all = list(product(
+    container_port_values,
+    host_port_values,
+    host_ip_values,
+    protocol_values,
+    app_protocol_values,
+    name_values,
+    mode_values,
+    force_long_syntax_values,
+))
+
+valid_ports = [t for t in prod_all if valid(t)] # type: ignore
+
+invalid_ports = [t for t in prod_all if not valid(t)] # type: ignore
 
 double_prod = random_choices(list(product(valid_ports, repeat = 2)), k = 100)
+
+container_port_1 = 80
+container_port_2 = (8000, 8010)
+host_port_1 = 8080
+host_port_2 = (9000, 9010)
+host_ip_1 = IPv4Address("192.168.0.1")
+host_ip_2 = IPv6Address("::1")
+protocol = "tcp"
+name = "mead"
+app_protocol = AppProtocol.http
+mode = "host"
+
+full_ports = [
+    ((container_port_1, None, None, None, None, None, None, False), "80"),
+    ((container_port_2, None, None, None, None, None, None, False), "8000-8010"),
+    ((container_port_1, host_port_1, None, None, None, None, None, False), "8080:80"),
+    ((container_port_2, host_port_2, None, None, None, None, None, False), "9000-9010:8000-8010"),
+    ((container_port_1, host_port_2, None, None, None, None, None, False), "9000-9010:80"),
+    ((container_port_2, None, host_ip_1, None, None, None, None, False), "192.168.0.1:8000-8010"),
+    ((container_port_2, host_port_2, host_ip_1, None, None, None, None, False), "192.168.0.1:9000-9010:8000-8010"),
+    ((container_port_2, None, host_ip_2, None, None, None, None, False), "::1:8000-8010"),
+    ((container_port_2, None, None, protocol, None, None, None, False), "8000-8010/tcp"),
+    ((container_port_1, host_port_1, host_ip_1, protocol, app_protocol, name, mode, False), {
+        "name": "mead",
+        "target": "80",
+        "host_ip": "192.168.0.1",
+        "published": "8080",
+        "protocol": "tcp",
+        "app_protocol": "http",
+        "mode": "host",
+    }),
+]
 
 ## CREATION
 
 @pytest.mark.parametrize("port", valid_ports)
 def test_create(port : TAll):
-    port_obj = create(*port)
-    assert (
-        port_obj.container_port,
-        port_obj.host_port,
-        port_obj.host_ip,
-        port_obj.protocol,
-        port_obj.name,
-        port_obj.app_protocol,
-        port_obj.mode,
-        port_obj.force_long_syntax,
-    ) == port
+    check_create_full(LABELS, port, create)
 
 
 @pytest.mark.xfail(raises = ValidationError)
 @pytest.mark.parametrize("port", invalid_ports)
 def test_create_fail(port : TAll):
-    create(*port)
+    create(port)
 
 ## API
 
 @pytest.mark.parametrize("port,expected", full_ports)
 def test_full_port(port : TAll, expected : Configuration):
-    assert create(*port).get_full_port() == expected
+    assert create(port).get_full_port() == expected
 
 ## EQUALITY
 
@@ -151,18 +151,17 @@ def test_full_port(port : TAll, expected : Configuration):
 def test_equal(port1 : TAll, port2 : TAll):
     container_port1, _, _, _, _, _, _, _ = port1
     container_port2, _, _, _, _, _, _, _ = port2
-    assert (create(*port1) == create(*port2)) == (container_port1 == container_port2)
+    assert (create(port1) == create(port2)) == (container_port1 == container_port2)
 
 ## HASH
 
 @pytest.mark.parametrize("port", valid_ports)
 def test_hash(port : TAll):
     container_port, _, _, _, _, _, _, _ = port
-    assert hash(create(*port)) == hash(container_port)
+    assert hash(create(port)) == hash(container_port)
 
 ## REPR
 
 @pytest.mark.parametrize("port", valid_ports)
 def test_repr(port : TAll):
-    container_port, host_port, host_ip, protocol, name, app_protocol, mode, force_long_syntax = port
-    assert repr(create(*port)) == f"{{'container_port': {repr(container_port)}, 'host_port': {repr(host_port)}, 'host_ip': {repr(host_ip)}, 'protocol': {repr(protocol)}, 'app_protocol': {repr(app_protocol)}, 'name': {repr(name)}, 'mode': {repr(mode)}, 'force_long_syntax': {repr(force_long_syntax)}}}"
+    check_repr_full(LABELS, port, create)
