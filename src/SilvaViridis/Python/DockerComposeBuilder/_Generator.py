5dc1a03c6521893cb9360f4978a145479dd36124
Maria Sorokina <ruafelianna@ya.ru>
2025-04-10 14:12:32 +0300

    Add volumes creation for volume type == volume

--- src/SilvaViridis/Python/DockerComposeBuilder/_Generator.py
+++ src/SilvaViridis/Python/DockerComposeBuilder/_Generator.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, ConfigDict
 from yaml import dump as to_yaml
 
 from .Common import ConfigurationDict
-from .Models import Container, Network
+from .Models import Container, Network, Volume, VolumeType
 
 class DockerComposeGenerator(BaseModel):
     containers : frozenset[Container]
@@ -15,10 +15,9 @@ class DockerComposeGenerator(BaseModel):
         self,
     ) -> str:
         categories : ConfigurationDict = {}
-
         services : ConfigurationDict = {}
-
         networks : set[Network] = set()
+        volumes : set[Volume] = set()
 
         for container in self.containers:
             services[container.container_name] = container.get_full_container()
@@ -26,9 +25,16 @@ class DockerComposeGenerator(BaseModel):
             for network in container.networks:
                 networks.add(network)
 
+            for volume in container.volumes:
+                if volume.volume_type == VolumeType.volume:
+                    volumes.add(volume)
+
         categories["services"] = services
 
         if len(networks) > 0:
             categories["networks"] = {n.name: {"external": "true"} for n in networks}
+
+        if len(volumes) > 0:
+            categories["volumes"] = {str(v.source): "" for v in volumes}
 
         return to_yaml(categories)
